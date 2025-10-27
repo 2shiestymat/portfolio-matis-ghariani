47cb59d935ae75f2d4580c0e525e95d5fc072f15
Matis Ghariani <2278673@cmontmorency.qc.ca>
2025-10-27 14:09:25 -0400

    37 - amelioration fetch

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const app = Vue.createApp({
     fetch("projects.json")
       .then((data) => data.json())
       .then((result) => (this.projects = result));
-  },
+  }
 });
 
 app.component("photo-gallery", {
@@ -49,3 +49,49 @@ app.component("photo-gallery", {
 });
 
 app.mount("#app");
+ 
+// Animations Gsap
+
+gsap.registerPlugin(ScrollToPlugin, SplitText, ScrollTrigger);
+
+function getSamePageAnchor(link) {
+  if (
+    link.protocol !== window.location.protocol ||
+    link.host !== window.location.host ||
+    link.pathname !== window.location.pathname ||
+    link.search !== window.location.search
+  ) {
+    return false;
+  }
+ 
+  return link.hash;
+}
+ 
+// Scroll to a given hash, preventing the event given if there is one
+function scrollToHash(hash, e) {
+  const elem = hash ? document.querySelector(hash) : false;
+  if (elem) {
+    if (e) e.preventDefault();
+    gsap.to(window, { scrollTo: elem });
+  }
+}
+ 
+// If a link's href is within the current page, scroll to it instead
+document.querySelectorAll("a[href]").forEach((a) => {
+  a.addEventListener("click", (e) => {
+    scrollToHash(getSamePageAnchor(a), e);
+  });
+});
+ 
+// Scroll to the element in the URL's hash on load
+scrollToHash(window.location.hash);
+
+// Animation du header principal nom et portfolio
+const titre = document.querySelector(".elementacceuil");
+gsap.timeline({ defaults: { duration: 1, ease: "power3.out" } })
+  .from(titre,
+    { opacity: 0,
+       scale: 0.8 // nom apparaît avec un effet scale
+    });
+
+
