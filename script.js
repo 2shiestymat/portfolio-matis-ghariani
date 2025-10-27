7dabd50dc64cdd1d9d2b191395ae6f2317308c42
2shiestymat <2278673@cmontmorency.qc.ca>
2025-10-27 00:51:05 -0400

    36 - composante vu

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const app = Vue.createApp({
   data() {
-    return { projects: [] };
+    return {
+      projects: [],
+      photos: [
+        { src: "img/export-1.jpg" },
+        { src: "img/export-2.jpg" },
+        { src: "img/export-3.jpg" },
+        { src: "img/export-4.jpg" },
+      ],
+    };
   },
   mounted() {
     fetch("projects.json")
@@ -8,4 +16,36 @@ const app = Vue.createApp({
       .then((result) => (this.projects = result));
   },
 });
+
+app.component("photo-gallery", {
+  props: ["images"],
+  data() {
+    return {
+      showGallery: false,
+      selectedImage: null,
+    };
+  },
+  template: `
+    <div>
+      <div class="btnjouer">
+       <button v-if="!showGallery" @click="showGallery = true" >
+        Ouvrir la galerie 
+       </button>
+      </div>
+
+      <div v-if="showGallery" class="gallery">
+        <img 
+          v-for="img in images"  
+          :src="img.src" 
+          @click="selectedImage = img"
+        >
+      </div>
+
+      <div v-if="selectedImage" class="modal" @click="selectedImage = null">
+        <img :src="selectedImage.src" :alt="selectedImage.title">
+      </div>
+    </div>
+  `,
+});
+
 app.mount("#app");
